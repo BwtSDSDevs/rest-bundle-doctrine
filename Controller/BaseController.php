0fdee4d950fa6513ab8ea3fc046edcdbc66fe3ed
Philip Washington Sorst <philip@sorst.net>
2015-01-29 16:44:56 +0100

    Adding setLocationHeader

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -3,6 +3,7 @@
 namespace Dontdrinkandroot\RestBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use JMS\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
@@ -44,11 +45,31 @@ class BaseController extends FOSRestController
      *
      * @return ConstraintViolationListInterface
      */
-    protected function validate($object) {
+    protected function validate($object)
+    {
         $validator = $this->get('validator');
         $errors = $validator->validate($object);
 
         return $errors;
     }
 
+    /**
+     * Generates a URL from the given parameters.
+     *
+     * @param View        $view
+     * @param string      $route         The name of the route
+     * @param mixed       $parameters    An array of parameters
+     * @param bool|string $referenceType The type of reference (one of the constants in UrlGeneratorInterface)
+     *
+     * @return View
+     */
+    protected function setLocationHeader(View $view, $route, $parameters, $referenceType)
+    {
+        $view->setHeader(
+            'Location',
+            $this->generateUrl($view, $parameters, $referenceType)
+        );
+
+        return $view;
+    }
 }
