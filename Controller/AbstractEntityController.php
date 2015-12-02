7bccc6e9de4b8d09046caabbb2b55d644f3c1199
Philip Washington Sorst <philip@sorst.net>
2015-12-02 01:10:18 +0100

    Refactoring AbstractEntityController

--- Controller/AbstractEntityController.php
+++ Controller/AbstractEntityController.php
@@ -5,6 +5,7 @@ namespace Dontdrinkandroot\RestBundle\Controller;
 
 use Dontdrinkandroot\Entity\EntityInterface;
 use Dontdrinkandroot\Repository\OrmEntityRepository;
+use Dontdrinkandroot\Utils\StringUtils;
 use Dontdrinkandroot\UtilsBundle\Controller\EntityControllerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,9 @@ abstract class AbstractEntityController extends BaseController implements Entity
 
     public function listAction(Request $request)
     {
+        $user = $this->getUser();
+        $this->checkListActionAuthorization($user);
+
         $page = $request->query->get('page', 1);
         $perPage = $request->query->get('perpage', 10);
         $paginatedEntities = $this->getRepository()->findPaginatedBy($page, $perPage);
@@ -33,13 +37,18 @@ abstract class AbstractEntityController extends BaseController implements Entity
     public function detailAction(Request $request, $id)
     {
         $entity = $this->fetchEntity($id);
+
+        $user = $this->getUser();
+        $this->checkDetailActionAuthorization($user, $entity);
+
+
         $view = $this->view($entity);
 
         return $this->handleView($view);
     }
 
     /**
-     * @param Request           $request
+     * @param Request $request
      * @param mixed|null|string $id
      *
      * @return Response
@@ -48,7 +57,18 @@ abstract class AbstractEntityController extends BaseController implements Entity
     {
         $create = (null === $id);
 
+        $user = $this->getUser();
+        $originalEntity = null;
+        if ($create) {
+            $this->checkCreateActionAuthorization($user);
+        } else {
+            $originalEntity = $this->fetchEntity($id);
+            $this->checkUpdateActionAuthorization($user, $originalEntity);
+        }
+
         $entity = $this->deserializeRequestContent($request, $this->getEntityClass());
+        $entity = $this->postProcessDeserializedEntity($entity, $originalEntity);
+
         $errors = $this->validate($entity);
         if (count($errors) > 0) {
             $view = $this->view($errors, 400);
@@ -78,7 +98,10 @@ abstract class AbstractEntityController extends BaseController implements Entity
 
     public function deleteAction(Request $request, $id)
     {
+        $user = $this->getUser();
         $entity = $this->fetchEntity($id);
+        $this->checkDeleteActionAuthorization($user, $entity);
+
         $this->getRepository()->remove($entity);
         $view = $this->view();
         $view->setStatusCode(204);
@@ -134,7 +157,7 @@ abstract class AbstractEntityController extends BaseController implements Entity
             return $this->routePrefix;
         }
 
-        list($bundle, $entityName) = $this->extractBundleAndEntityName($this->getEntityClass());
+        list($bundle, $entityName) = $this->extractBundleAndEntityName();
 
         $prefix = str_replace('Bundle', '', $bundle);
         $prefix = $prefix . '.' . $entityName;
@@ -153,17 +176,17 @@ abstract class AbstractEntityController extends BaseController implements Entity
             return $this->pathPrefix;
         }
 
-        list($bundle, $entityName) = $this->extractBundleAndEntityName($this->getEntityClass());
+        list($bundle, $entityName) = $this->extractBundleAndEntityName();
 
         return '/' . strtolower($entityName) . '/';
     }
 
-    protected function extractBundleAndEntityName($entityClass)
+    protected function extractBundleAndEntityName()
     {
-        $entityClass = $this->getEntityClass();
-        $parts = explode(':', $entityClass);
+        $shortName = $this->getEntityShortName();
+        $parts = explode(':', $shortName);
         if (2 !== count($parts)) {
-            throw new \Exception(sprintf('Expecting entity class to be "Bundle:Entity", %s given', $entityClass));
+            throw new \Exception(sprintf('Expecting entity class to be "Bundle:Entity", %s given', $shortName));
         }
 
         return $parts;
@@ -175,6 +198,64 @@ abstract class AbstractEntityController extends BaseController implements Entity
     protected function getDetailRoute()
     {
         return $this->getRoutePrefix() . ".detail";
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEntityShortName()
+    {
+        $entityClass = $this->getEntityClass();
+        $entityClassParts = explode('\\', $entityClass);
+
+        $bundle = $this->findBundle($entityClassParts);
+        $className = $entityClassParts[count($entityClassParts) - 1];
+
+        $shortName = $bundle . ':' . $className;
+
+        return $shortName;
+    }
+
+    private function findBundle(array $entityClassParts)
+    {
+        foreach ($entityClassParts as $part) {
+            if (StringUtils::endsWith($part, 'Bundle')) {
+                return $part;
+            }
+        }
+
+        throw new \RuntimeException('No Bundle found in namespace');
+    }
+
+    /**
+     * @param EntityInterface $entity
+     * @param EntityInterface $originalEntity
+     *
+     * @return EntityInterface
+     */
+    protected function postProcessDeserializedEntity(EntityInterface $entity, EntityInterface $originalEntity = null)
+    {
+        return $entity;
+    }
+
+    protected function checkListActionAuthorization($user)
+    {
+    }
+
+    protected function checkDetailActionAuthorization($user, EntityInterface $entity)
+    {
+    }
+
+    protected function checkCreateActionAuthorization($user)
+    {
+    }
+
+    protected function checkUpdateActionAuthorization($user, EntityInterface $entity)
+    {
+    }
+
+    protected function checkDeleteActionAuthorization($user, EntityInterface $entity)
+    {
     }
 
     /**
