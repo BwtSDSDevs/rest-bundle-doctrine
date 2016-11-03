5368b7a995e2a730b5d1ba778ad106edb5394f81
Philip Washington Sorst <philip@sorst.net>
2016-11-03 15:17:32 +0100

    Enhancing DateTimeSubscriber

--- Form/DateTimeSubscriber.php
+++ Form/DateTimeSubscriber.php
@@ -6,6 +6,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\ResolvedFormTypeInterface;
 
 class DateTimeSubscriber implements EventSubscriberInterface
 {
@@ -23,8 +25,7 @@ class DateTimeSubscriber implements EventSubscriberInterface
         $children = $form->all();
         $dateTimeFields = [];
         foreach ($children as $child) {
-            $prefix = $child->getConfig()->getType()->getBlockPrefix();
-            if ('datetime' === $prefix) {
+            if ($this->isDateTimeType($child->getConfig()->getType())) {
                 $dateTimeFields[] = $child->getName();
             }
         }
@@ -36,5 +37,20 @@ class DateTimeSubscriber implements EventSubscriberInterface
         foreach ($dateTimeFields as $dateTimeField) {
             $form->add($dateTimeField, DateTimeType::class, ['widget' => 'single_text']);
         }
+    }
+
+    private function isDateTimeType(ResolvedFormTypeInterface $type)
+    {
+        $prefix = $type->getBlockPrefix();
+        if (in_array($prefix, ['datetime'])) {
+            return true;
+        }
+
+        $parent = $type->getParent();
+        if (null !== $parent) {
+            return $this->isDateTimeType($parent);
+        }
+
+        return false;
     }
 }
